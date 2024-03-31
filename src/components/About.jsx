3cacf00abd59e72b7ea3b18b23952a96161e3625
TimiMaliki <malikitimi@gmail.com>
2024-03-31 13:57:36 +0100

    added:rewriting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <section className="w-full h-full  grid lg:grid-cols-3 lg:p-12">
       <div className="card-one w-full h-full  text-center">
         <h1 className="text-5xl lg:text-7xl font-extrabold mt-2 p-8">
-          Creating Blog for Leading Industries
+        Empowering Leading Industries with Maliki-Studio
         </h1>
       </div>
 
@@ -15,17 +15,11 @@ const About = () => {
 
       <div className="card-one w-full h-full  text-center">
         <p className="text-xl   p-10">
-          At Maliki-Studio, we're dedicated to unlocking your brand's full
-          potential through cutting-edge digital solutions. <br /> <br /> Our expertise spans
-          UX/UI Design, Branding, and E-commerce, focusing on creating impactful
-          digital experiences within Healthcare, SaaS, FinTech, Real Estate,
-          E-commerce, and AI industries. <br /> <br /> We prioritize working within your
-          budget, strategically planning and rationalizing expenditures to
-          deliver a cost-effective digital strategy. Our goal is to provide you
-          with a profitable product strategy and an efficient execution plan,
-          maximizing your investment's return. Partner with us to make your
-          brand a beacon in its industry, thriving in the dynamic online
-          landscape.
+        "At Maliki-Studio, we specialize in unleashing the full potential of your brand through advanced digital solutions. <br /> <br /> 
+        With a focus on UX/UI Design, Branding, and E-commerce, our expertise is tailored to create impactful digital experiences across Healthcare, SaaS, FinTech, Real Estate, E-commerce, and AI sectors. <br /> <br /> 
+        Our approach is budget-conscious, meticulously planning and rationalizing expenses to deliver a cost-effective digital strategy. 
+        Our aim is to provide you with a profitable product strategy and an efficient execution plan, ensuring maximum returns on your investment. 
+        Partner with us to position your brand as a leader in its industry, thriving in the ever-evolving online landscape.
         </p>
       </div>
     </section>
